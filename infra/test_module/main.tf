6fb4e91680d726e2a258fc4ad65bbaaec968af5b
08Aristodemus24 <MichaelAveuc571@gmail.com>
2025-07-27 22:47:27 +0800

    update

--- infra/test_module/main.tf
+++ infra/test_module/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_user_assigned_identity" "uai" {
   name                = "${var.project_name}uai"
   resource_group_name = "${var.project_name}rg"
 
-  depends_on           = [azurerm_resource_group.rg]
+  # depends_on           = [ azurerm_resource_group.rg ]
 }
 
 output "principal_id" {
